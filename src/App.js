968690e6cb1d4c9dae4fcc68a1aaeebc6fe12224
Bladerama <patrick_wauschek@mailbox.org>
2020-10-28 12:22:14 -0500

    App.js is now a class and component.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,6 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
 
-const games = [
-  {
-    name: 'Team Fortress 2',
-    appId: 440,
-  },
-  {
-    name: 'Counter-Strike: Source',
-    appId: 123,
-  }
-];
-
 // const gamesList = document.getElementById('games-list');
 // const searchBar = document.getElementById('search-bar');
 // searchBar.addEventListener('keyup', (e) => {
@@ -38,31 +27,44 @@ const games = [
 //   gamesList.innerHTML = htmlString;
 // };
 
-function App() {
-  return (
-    // <div>
-    //   {games.filter(game => game.appId === 440).map(filteredGame => (
-    //     <li>
-    //       {filteredGame.name}
-    //     </li> */}
-    //   {/* ))}
-    // </div>
-    <div class="achieveRow ">
-      <div class="achieveImgHolder">
-        <img src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg"
-          alt="achievement icon" width="64" height="64" border="0"></img>
-      </div>
-      <div class="achieveTxtHolder">
-        <div class="achieveFill" style={{width: '55%'}}>
+const games = [
+  {
+    name: 'Team Fortress 2',
+    appId: 440,
+  },
+  {
+    name: 'Counter-Strike: Source',
+    appId: 123,
+  }
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      // <div>
+      //   {games.filter(game => game.appId === 440).map(filteredGame => (
+      //     <li>
+      //       {filteredGame.name}
+      //     </li> */}
+      //   {/* ))}
+      // </div>
+      <div class="achieveRow ">
+        <div class="achieveImgHolder">
+          <img src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg"
+            alt="achievement icon" width="64" height="64" border="0"></img>
         </div>
-        <div class="achievePercent">55.0%</div>
-        <div class="achieveTxt">
-          <h3>Race for the Pennant</h3>
-          <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laudantium, totam, nostrum id ad sunt vero provident obcaecati reiciendis consequuntur quo</h5>
+        <div class="achieveTxtHolder">
+          <div class="achieveFill" style={{ width: '55%' }}>
+          </div>
+          <div class="achievePercent">55.0%</div>
+          <div class="achieveTxt">
+            <h3>Race for the Pennant</h3>
+            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laudantium, totam, nostrum id ad sunt vero provident obcaecati reiciendis consequuntur quo</h5>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
