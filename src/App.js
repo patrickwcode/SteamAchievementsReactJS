4a04ece50735584104d42c363fe7cf8ff7599c0f
Bladerama <patrick_wauschek@mailbox.org>
2020-10-29 11:57:17 -0500

    New styles added to match Steam Community Achievements website.

--- src/App.js
+++ src/App.js
@@ -40,30 +40,7 @@ const games = [
 
 class App extends React.Component {
   render() {
-    return (
-      // <div>
-      //   {games.filter(game => game.appId === 440).map(filteredGame => (
-      //     <li>
-      //       {filteredGame.name}
-      //     </li> */}
-      //   {/* ))}
-      // </div>
-      <div class="achieveRow ">
-        <div class="achieveImgHolder">
-          <img src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg"
-            alt="achievement icon" width="64" height="64" border="0"></img>
-        </div>
-        <div class="achieveTxtHolder">
-          <div class="achieveFill" style={{ width: '55%' }}>
-          </div>
-          <div class="achievePercent">55.0%</div>
-          <div class="achieveTxt">
-            <h3>Race for the Pennant</h3>
-            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laudantium, totam, nostrum id ad sunt vero provident obcaecati reiciendis consequuntur quo</h5>
-          </div>
-        </div>
-      </div>
-    );
+    return (<div></div>);
   }
 }
 
