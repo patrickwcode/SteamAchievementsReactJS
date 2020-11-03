5417e9a3924ef46899d368011f187a1fa4f21554
Bladerama <patrick_wauschek@mailbox.org>
2020-11-02 19:32:31 -0600

    Achievements style updated and needs work for mobile. TF2 image added below search.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
 
-// const gamesList = document.getElementById('games-list');
-// const searchBar = document.getElementById('search-bar');
-// searchBar.addEventListener('keyup', (e) => {
-//   const searchString = e.target.value.toLowerCase();
-// });
-// const filteredGames = games.filter(game => { 
-//   return (
-//     game.name.includes(searchString)
-//   );
-// })
-// displayGames(filteredGames);
-// });
-
-// const displayGames = (games) => {
-//   const htmlString = games.map((game) => {
-//           return `
-//           <li class="game">
-//               <h2>${game.name}</h2>
-//           </li>
-//       `;
-//       })
-//       .join('');
-//   gamesList.innerHTML = htmlString;
-// };
-
-const games = [
+const achievements = [
   {
-    name: 'Team Fortress 2',
-    appId: 440,
+    name: 'Race for the Pennant',
+    description: 'Run 25 kilometers.',
+    percent: 54.9,
+    iconUrl: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg'
   },
   {
-    name: 'Counter-Strike: Source',
-    appId: 123,
-  }
+    name: 'Race for the Pennant',
+    description: 'Run 25 kilometers.',
+    percent: 54.9,
+    iconUrl: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg'
+  },
+  {
+    name: 'Race for the Pennant',
+    description: 'Run 25 kilometers.',
+    percent: 54.9,
+    iconUrl: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg'
+  },
+  {
+    name: 'Race for the Pennant',
+    description: 'Run 25 kilometers.',
+    percent: 54.9,
+    iconUrl: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg'
+  },
+  {
+    name: 'Race for the Pennant',
+    description: 'Run 25 kilometers.',
+    percent: 54.9,
+    iconUrl: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/440/924764eea604817d3c14de9640ae6422c7cdfb7a.jpg'
+  },
 ];
 
-class App extends React.Component {
-  render() {
-    return (<div></div>);
-  }
+function GameAchievement(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.description}</h1>
+      <h1>{props.percent}</h1>
+      <h1>{props.iconUrl}</h1>
+    </div>
+  )
 }
+
+function App() {
+  return (
+    <ol>
+      { achievements.map(achievement => (
+        <li key={achievement.name}>
+          { <GameAchievement name={achievement.name} /> }
+          { <GameAchievement description={achievement.description} /> }
+          { <GameAchievement percent={achievement.percent} /> }
+          { <GameAchievement iconUrl={achievement.iconUrl} /> }
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+// ReactDOM.render(<App />, document.getElementById('achievements-container'));
 
 export default App;
