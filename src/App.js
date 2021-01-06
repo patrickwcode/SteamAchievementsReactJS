fb1f11b8079d43400637a3b2afbe0cd923a67ec7
Bladerama <patrick_wauschek@mailbox.org>
2021-01-06 08:34:34 -0600

    Component and Fetch Working
    Data is loading correctly. Page will lag badly from fetches and / or data rendering to screen.

--- src/App.js
+++ src/App.js
@@ -1,85 +1,45 @@
 import React from 'react';
-import './App.css';
-import Modal from "./Modal";
-import useModal from './useModal';
-// import achievements from './achievements.json';
-// import APIService from './APIService.js';
 import GameAchievement from './GameAchievement';
+import useModal from './useModal';
+import './App.css';
 
-// function componentDidMount() {
-//   APIService.getAchievements().then((data) => {
-//     console.log(data);
-//   })
-//     .catch(function (ex) {
-//       console.log('Response parsing failed. Error: ', ex);
-//     });;
-// }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-// let url = 'http://localhost:8080/tf2';
-// const componentDidMount = async () => {
-// let response = await fetch(url);
-// return achievements = await response.json();
-// }
+        this.state = {
+            achievements: [],
+        };
 
-const params = {
-  method: 'GET',
-  headers: {
-    'accept': 'application/json'
+        this.getAchievements = this.getAchievements.bind(this);
+    }
+
+    async getAchievements() {
+        const res = await fetch(`http://localhost:8080/tf2`);
+        const data = await res.json();
+        return data;
+    }
+
+    async componentDidMount() {
+        const achievements = await this.getAchievements();
+        this.setState({ achievements });
+    }
+
+    render() {
+      this.componentDidMount();
+      return (
+          <div>
+              {this.state.achievements.map((achievement) => (
+                  <GameAchievement
+                      name={achievement.achievementName}
+                      description={achievement.achievementDescription}
+                      percent={achievement.achievementPercent}
+                      iconUrl={achievement.achievementIconUrl}
+                  />
+              ))}
+          </div>
+      );
   }
-};
-
-function componentDidMount() {
-  fetch('http://localhost:8080/tf2', params)
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(e => console.error(e));
 }
-
-const achievements = componentDidMount();
-
-// async function getAchievements() {
-//   return await
-//   fetch('http://localhost:8080/tf2')
-//   .then(resp => resp.json());
-// }
-
-// const achievements = getAchievements();
-
-const App = () => {
-  const { isVisible, toggleModal } = useModal();
-  return (
-    <div>
-
-      {
-        <div>
-          {achievements}
-        </div>
-        // <ol>
-        //   {achievements.map(achievement => (
-        //     <li key={achievement.name}>
-        //       { <GameAchievement name={achievement.name}
-        //         description={achievement.description}
-        //         percent={achievement.percent}
-        //         icon={achievement.iconUrl}
-        //       />}
-
-        //       <Modal key={achievement.name}
-        //         isVisible={isVisible}
-        //         hideModal={toggleModal}
-        //         achievementName={achievement.name}
-        //         achievementPercent={achievement.percent}
-        //         achievementDescription={achievement.description}
-        //         achievementIconUrl={achievement.iconUrl} />
-
-        //       <button onClick={toggleModal}>
-        //         Show modal
-        //     </button>
-        //     </li>
-        //   ))}
-        // </ol>
-      }
-    </div>
-  );
-};
 
 export default App;
