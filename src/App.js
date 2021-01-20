f4831a41d304b0d56f949b53626ca0e957dccbb7
Bladerama <patrick_wauschek@mailbox.org>
2021-01-19 19:49:47 -0600

    Pagination Implemented
    Uses react-paginate library.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPaginate from 'react-paginate';
 import GameAchievement from './GameAchievement';
 import useModal from './useModal';
 import './App.css';
@@ -9,37 +10,72 @@ class App extends React.Component {
 
         this.state = {
             achievements: [],
+            offset: 0,
+            perPage: 10,
+            currentPage: 0
         };
-
-        this.getAchievements = this.getAchievements.bind(this);
     }
 
     async getAchievements() {
         const res = await fetch(`http://localhost:8080/tf2`);
         const data = await res.json();
-        return data;
+        const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage);
+        const achievements = slice.map(achievement =>
+            <GameAchievement
+                key={achievement.achievementName}
+                name={achievement.achievementName}
+                description={achievement.achievementDescription}
+                percent={achievement.achievementPercent}
+                iconUrl={achievement.achievementIconUrl}
+            />
+        )
+
+        this.setState({
+            pageCount: Math.ceil(data.length / this.state.perPage),
+
+            achievements
+        })
     }
 
     async componentDidMount() {
-        const achievements = await this.getAchievements();
-        this.setState({ achievements });
+        this.getAchievements();
     }
 
+    handlePageChange(pageNumber) {
+        console.log(`active page is ${pageNumber}`);
+        this.setState({ activePage: pageNumber });
+    }
+
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset,
+        }, () => {
+            this.getAchievements()
+        });
+    };
     render() {
-      this.componentDidMount();
-      return (
-          <div>
-              {this.state.achievements.map((achievement) => (
-                  <GameAchievement
-                      name={achievement.achievementName}
-                      description={achievement.achievementDescription}
-                      percent={achievement.achievementPercent}
-                      iconUrl={achievement.achievementIconUrl}
-                  />
-              ))}
-          </div>
-      );
-  }
+        return (
+            <div>
+                {this.state.achievements}
+                <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/GameAchievement.js
+++ src/GameAchievement.js
@@ -2,25 +2,31 @@ import React from 'react'
 
 export default function GameAchievement(achievement) {
   return (
-    //   <div>
-    //   <h1>{achievement.name}</h1>
-    //   {achievement.description}
-    //   {achievement.percent}
-    //   {achievement.iconUrl}
-    // </div>
-    <div class="achieveRow">
-      <div class="achieveImgHolder">
-        <img src={achievement.iconUrl}
-          alt="Achievement Logo" width="64" height="64" border="0" />
-      </div>
-      <div class="achieveTxtHolder">
-        <div class="achieveFill" style={{width: `${achievement.percent}%`}}>
+
+    //   num.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+    // That will fix your percentage display issue
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString 
+
+    // The instructions on how to use it are towards the bottom of that page.ctrl + f for that line of code at the beginning of this message
+
+    // LOOK INTO PAGINATION FOR RENDERING ACHIEVEMENTS
+    <div>
+      <div className="achieveRow" >
+        <div className="achieveImgHolder">
+          <img src={achievement.iconUrl}
+            alt="Achievement Logo" width="64" height="64" border="0" />
         </div>
-        <div class="achievePercent">{achievement.percent}%</div>
-        <div class="achieveTxt">
-          <h3>{achievement.name}</h3>
-          <h5>{achievement.description}
-              </h5>
+        <div className="achieveTxtHolder">
+          <div className="achieveFill" style={{ width: `${achievement.percent}%` }}>
+          </div>
+          <div className="achievePercent">{achievement.percent}%</div>
+          <div className="achieveTxt">
+            <h3>{achievement.name}</h3>
+            <h5>{achievement.description}
+            </h5>
+          </div>
         </div>
       </div>
     </div>
