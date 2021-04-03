539f2d5912b898e38c99fdb718e9b612a6305554
Bladerama <patrick_wauschek@mailbox.org>
2021-04-02 21:09:29 -0500

    Fixes Modal Display & Close Button

--- src/Modal.js
+++ src/Modal.js
@@ -5,23 +5,23 @@ import './Modal.css';
 const Modal = ({ name, description, percent, iconUrl, isVisible, hideModal }) => {
 
   return isVisible
-  ? createPortal(
-    <div class="modal">
-      <div class="content">
-        <img class="icon" src={iconUrl} alt={name} width="80px" height="80px"/>
-        <h1>{name}</h1>
-        <h1>{Math.round(10*percent)/10}%</h1>
-        <h4>
-          {description}
-        </h4>
-      </div>
-      <button class="close" onClick={hideModal}>
-        X
-      </button>
-    </div>,
-    document.body,
-  )
-  : null;
+    ? createPortal(
+      <div class="modal">
+        <button class="close" onClick={hideModal}>
+          X
+        </button>
+        <div class="content">
+          <img class="icon" src={iconUrl} alt={name} width="80px" height="80px" />
+          <h1>{name}</h1>
+          <h1>{Math.round(10 * percent) / 10}%</h1>
+          <h4>
+            {description}
+          </h4>
+        </div>
+      </div>,
+      document.body,
+    )
+    : null;
 };
 
 export default Modal;
